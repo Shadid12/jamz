eaa18a009f92f38ccebd5e24e23b7dd948e51d42
shadid12 <shadidhaque2012@gmail.com>
2017-11-20 00:51:42 -0500

    ready to add new user auth redux

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import FacebookLogin from 'react-facebook-login';
 class Home extends React.Component {
     render() {
         return (
-            <div>Hello home</div>
+            <div>
+                <FacebookLogin
+                    appId="1755048537846980"
+                    autoLoad={true}
+                    fields="name,email,picture"
+                    onClick={() => {
+                        console.log('ahha');
+                    }}
+
+                    callback={this.responseFacebook} />
+            </div>
         )
+    }
+    responseFacebook = (response) => {
+        console.log(response);
     }
 }
 
