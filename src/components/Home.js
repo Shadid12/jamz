f1b2a9d896a7653c8f3af0b49dc2ee2d2f2766e9
shadid12 <shadidhaque2012@gmail.com>
2017-11-20 03:08:38 -0500

    ready to bring in profile component

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,21 +3,29 @@ import FacebookLogin from 'react-facebook-login';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {facebookAuth} from "../actions/index";
+import TiSocialFacebookCircular from 'react-icons/lib/ti/social-facebook-circular';
 
 class Home extends React.Component {
     render() {
-        return (
-            <div>
-                <FacebookLogin
-                    appId="1755048537846980"
-                    autoLoad={true}
-                    fields="name,email,picture"
-                    onClick={() => {
-                        console.log('ahha');
-                    }}
-                    callback={this.responseFacebook} />
-            </div>
-        )
+        if (this.props.facebook_auth) {
+            return (
+                <div>Logged in user</div>
+            )
+        } else {
+            return (
+                <div>
+                    <FacebookLogin
+                        appId="1755048537846980"
+                        autoLoad={true}
+                        fields="name,email,picture"
+                        icon={<TiSocialFacebookCircular />}
+                        onClick={() => {
+                            console.log('ahha');
+                        }}
+                        callback={this.responseFacebook} />
+                </div>
+            )
+        }
     }
     responseFacebook = (response) => {
         this.props.facebookAuth(response);
